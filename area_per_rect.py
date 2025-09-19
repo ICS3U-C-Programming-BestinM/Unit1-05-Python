3aafaea40f90866bb1dde09aec7a1af861736ab2
Bestin Binu Mathew <bestin.mathew@stu.ocsb.ca>
2025-09-19 18:05:25 +0000

    updated

---
+++ area_per_rect.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Created By: Bestin Mathew
+# Date: 10:18:24
+# This program calculates area and perimeter of 5cm × 3cm rectangle.
+
+
+def main():
+    print("For a rectangle with length 5cm and width 3cm:")
+    print()
+    print("The area is: {}cm^2".format(5 * 3))
+    print("The perimeter is: {}cm".format(2 * (5 + 3)))
+
+
+if __name__ == "__main__":
+    main()
